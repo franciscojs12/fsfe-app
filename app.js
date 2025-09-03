a0da98b754c490350527da21c88bcc010e01d071
Francisco Santillan <franjs12@gmail.com>
2025-09-02 23:07:53 -0300

    Change to port 3001

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // BASIC HTTP SERVER
 const http = require('http');
 
-const PORT = 3000;
+const PORT = 3001;
 
 http
   .createServer(function (req, res) {
-    res.write('HTTP response from the server');
+    res.write('HTTP response from the server (port 3001)');
     res.end();
   })
   .listen(PORT);
